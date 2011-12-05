5b64f5fbc32be6c855e076081fa13a358da1ac7f
airsun <airsun@airsun-MS-7529.(none)>
2011-12-05 15:50:26 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backlog::Application.config.secret_token = '947154894088ee729b4e056a33b95eecdd9a95e28d259f4779fae7c4eb8fd9dd73f6de56b53bff2c1a284bf011f7edb4afe00442da2832b6f9b1105e9e8b4592'
